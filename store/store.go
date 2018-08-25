897e6cd66d29456cedb34782504d01aceabf20e8
dewey <mail@notmyhostna.me>
2018-08-25 19:01:54 +0200

    Move config & add backend factory

--- store/store.go
+++ store/store.go
@@ -1,5 +1,7 @@
 package store
 
+import "github.com/dewey/feedbridge/config"
+
 // StorageRepository defines the interface for a k/v backend
 type StorageRepository interface {
 	// Save stores an item in the store
@@ -7,4 +9,24 @@ type StorageRepository interface {
 
 	// Get item from store
 	Get(string) (string, error)
+}
+
+// NewStoreBackend is a factory to return a store implementation based on the config choosen
+func NewStoreBackend(cfg config.Config) (*StorageRepository, error) {
+	var storageRepo *StorageRepository
+	switch cfg.StorageBackend {
+	case "memory":
+		memory, err := NewMemRepository(config.CacheExpiration, config.CacheExpiredPurge)
+		if err != nil {
+			return nil, err
+		}
+		storageRepo = memory
+	case "persistent":
+		disk, err := NewDiskRepository(config.StoragePath)
+		if err != nil {
+			return nil, err
+		}
+		storageRepo = disk
+	}
+	return storageRepo, nil
 }
