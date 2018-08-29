214f7e8c172e18a56b2e13485e4a2896a4b2e93c
dewey <mail@notmyhostna.me>
2018-08-29 22:05:10 +0200

    Add docker-compose examples for memory and persistant storage

--- config/config.go
+++ config/config.go
@@ -6,7 +6,7 @@ type Config struct {
 	CacheExpiration   int    `env:"CACHE_EXPIRATION" envDefault:"30"`
 	CacheExpiredPurge int    `env:"CACHE_EXPIRED_PURGE" envDefault:"60"`
 	StorageBackend    string `env:"STORAGE_BACKEND" envDefault:"memory"`
-	StoragePath       string `env:"STORAGE_PATH" envDefault:"feedbridge-data"`
+	StoragePath       string `env:"STORAGE_PATH" envDefault:"/feedbridge-data"`
 	Environment       string `env:"ENVIRONMENT" envDefault:"develop"`
 	Port              int    `env:"PORT" envDefault:"8080"`
 }
